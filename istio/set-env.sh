925fc5504d320c83c8728ea27a33e752eb3677d4
guoxudong <guoxudong@keking.cn>
2020-04-08 14:19:03 +0800

    fix

--- istio/set-env.sh
+++ istio/set-env.sh
@@ -1,3 +1,10 @@
-curl -L https://raw.githubusercontent.com/istio/istio/release-1.5/release/downloadIstioCandidate.sh | sh -
+#!/bin/bash
+
+sudo grep -i "done" curl -L https://raw.githubusercontent.com/istio/istio/release-1.5/release/downloadIstioCandidate.sh | sh - &> /dev/null
+if [[ "$?" -eq 0 ]]; then
+exit
+fi
+
 export PATH=$PATH:/root/istio-1.5.1/bin
+
 istioctl manifest apply --set profile=demo

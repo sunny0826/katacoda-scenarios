c672e4080fdc8f0994734f3030e4dd10decefb2a
guoxudong <guoxudong@keking.cn>
2020-04-30 15:46:04 +0800

    fix

--- docker-sync/assets/docker-sync.sh
+++ docker-sync/assets/docker-sync.sh
@@ -51,8 +51,7 @@ get_orgion_name() {
 change_image_name() {
     docker_name=$(get_image_name ${line})
     docker_tag=$(get_tag ${line})
-    # printf '%s' $USERNAME/$docker_name
-    printf '%s' registry.cn-shanghai.aliyuncs.com/keking/$docker_name:$docker_tag
+    printf '%s' $USERNAME/$docker_name:$docker_tag
 }
 
 get_image_id() {
@@ -76,6 +75,6 @@ run() {
 {
     verify_docker
     read_user_name
-    docker login --username=$USERNAME registry.cn-shanghai.aliyuncs.com
+    docker login -u $USERNAME
     run
 }

bf8050314791130796dc35669e952013af1a23fe
guoxudong <guoxudong@keking.cn>
2020-04-30 14:21:00 +0800

    fix

--- docker-sync/assets/docker-sync.sh
+++ docker-sync/assets/docker-sync.sh
@@ -63,7 +63,8 @@ run() {
 	cat "${IMAGE_LIST_FILE}" | while read line
     do
         sudo docker pull ${line}
-        # echo ${line}
+        echo $(get_image_id ${line})
+        echo $(change_image_name ${line})
         sudo docker tag $(get_image_id ${line}) $(change_image_name ${line})
         sudo docker push $(change_image_name ${line})
         # echo $(change_image_name ${line})
